5e3212394b3dc5f33bc453b435ac9126566c94fd
IVAN MASTRANGE <75397473+IvanMCarval@users.noreply.github.com>
2022-06-21 18:06:39 -0300

    Primeira config da api usando GraphCMS e Apollo

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { gql, useQuery } from "@apollo/client"
+import { useEffect } from "react"
+import { client } from "./lib/apollo"
+
+const GET_LESSONS_QUERY = gql`
+  query{
+    lessons{
+      id
+      title
+    }
+  }
+`
+interface Lesson{
+  id: string;
+  title: string;
+}
 
 function App() {
+  const {data} = useQuery<{lessons: Lesson[]}>(GET_LESSONS_QUERY)
 
   return (
-    <h1>Hellow World</h1>
+    <ul>
+      {data?.lessons.map(lesson => {
+        return <li key={lesson.id}>{lesson.title}</li>
+      })}
+    </ul>
   )
 }
 
