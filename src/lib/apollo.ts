698f93903a41e117e4a781dddb5ef1d4788c9f5d
IVAN MASTRANGE <75397473+IvanMCarval@users.noreply.github.com>
2022-06-25 11:17:59 -0300

    Alterado identação

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-    uri: 'https://api-sa-east-1.graphcms.com/v2/cl4olxbvs0dkd01xtbl5f23pr/master',
-    cache: new InMemoryCache()
-})
+  uri: "https://api-sa-east-1.graphcms.com/v2/cl4olxbvs0dkd01xtbl5f23pr/master",
+  cache: new InMemoryCache(),
+});
